abe5f1ba9c06df8cf07aee4b436fad3d6660c67d
Vitor <vitorhugosantos144@gmail.com>
2022-04-19 09:48:43 +0100

    javadoc added

--- src/edu/ufp/inf/en/siu/Node.java
+++ src/edu/ufp/inf/en/siu/Node.java
@@ -88,12 +88,22 @@ public class Node {
     }
   }
 
+  /**
+   * adds a tag to node
+   * @param t tag
+   * @param value value associated with the tag (e.g. BUILDING=TRUE)
+   */
   public void addTag(Tag t, String value) {
     if (t == null) throw new IllegalArgumentException("argument 't' to addTag() is null");
     if (value == null) throw new IllegalArgumentException("argument 'value' to addTag() is null");
     this.tags.put(t, value);
   }
 
+  /**
+   * removes a tag from node
+   * @param t tag to remove
+   * @return tag removed || null if not found
+   */
   public Tag removeTag(Tag t) {
     if (t == null) throw new IllegalArgumentException("argument to removeTag() is null");
     if (this.tags.contains(t)) {
@@ -106,12 +116,21 @@ public class Node {
     }
   }
 
+  /**
+   * edits a tag from node
+   * @param o old tag to remove
+   * @param n new tag to insert
+   * @param nValue value of new tag (e.g. BUILDING=TRUE)
+   */
   public void editTag(Tag o, Tag n, String nValue) {
     if (o == null) throw new IllegalArgumentException("argument 'o' to editTag() is null");
     if (n == null) throw new IllegalArgumentException("argument 'n' to editTag() is null");
     if (removeTag(o) != null) addTag(n, nValue);
   }
 
+  /**
+   * lists all tags in node
+   */
   public void listTags() {
     if (!this.tags.isEmpty()) {
       for (Tag tag : this.tags.keys()) {
@@ -120,6 +139,11 @@ public class Node {
     }
   }
 
+  /**
+   * checks if node contains a certain tag
+   * @param t tag to search
+   * @return true if found || false if not 
+   */
   public boolean containsTag(Tag t) {
     return this.tags.contains(t);
 }
