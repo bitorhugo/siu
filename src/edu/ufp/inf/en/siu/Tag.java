63a7f2dccb2abc2a74ec35dc2264c38da186c307
Vitor <vitorhugosantos144@gmail.com>
2022-04-19 20:05:44 +0100

    enum updated

--- src/edu/ufp/inf/en/siu/Tag.java
+++ src/edu/ufp/inf/en/siu/Tag.java
@@ -2,14 +2,30 @@ package edu.ufp.inf.en.siu;
 
 
 public enum Tag {
-    BUILDING,
-    ROAD,
-    LANES,
-    ACCESS,
-    SURFACE,
-    ADDRESS,
-    BIKELANE,
-    ONEWAY,
-    BUS,
-    MAXSPEED
+    BUILDING("default"),
+    ROAD("default"),
+    LANES("default"),
+    ACCESS("default"),
+    SURFACE("default"),
+    ADDRESS("default"),
+    BIKELANE("default"),
+    ONEWAY("default"),
+    BUS("default"),
+    MAXSPEED("default");
+
+    private String value;
+
+    Tag (String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    
 }
