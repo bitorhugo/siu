92a5b5c0f0b4819a5f68e1c349b6baf6c3553150
Vitor <vitorhugosantos144@gmail.com>
2022-05-02 02:28:42 +0100

    added timeWeights to Way

--- src/edu/ufp/inf/en/siu/Way.java
+++ src/edu/ufp/inf/en/siu/Way.java
@@ -1,5 +1,6 @@
 package edu.ufp.inf.en.siu;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 import edu.princeton.cs.algs4.DirectedEdge;
@@ -9,9 +10,26 @@ import edu.princeton.cs.algs4.SeparateChainingHashST;
 
 public class Way extends DirectedEdge {
 
+  /**
+   * average speed at which each means of transport travel (measured in meters/minute)
+   */
+  private enum TransportSpeed {
+    WALKINGSPEED(80),
+    CYCLINGSPEED(308.33),
+    CITYSPEED(833.333),
+    HIGHWAYSPEED(1833.33), 
+    BUSSPEED(750);
+
+    final double value;
+    private TransportSpeed (double value){
+      this.value = value;
+    }    
+  }
+
   private final Integer wayId;
   private SeparateChainingHashST<Tag, String> tags = new SeparateChainingHashST<>();
-  private SeparateChainingHashST<String, Double> weights = new SeparateChainingHashST<>();
+  // key: transportSpeed, value: timeWeight (minutes)
+  private SeparateChainingHashST<TransportSpeed, Double> timeWeights = new SeparateChainingHashST<>();
   
   // add time-weights here aswell
   // car, walk, bus, ...
@@ -21,6 +39,10 @@ public class Way extends DirectedEdge {
   public Way(Integer wayId, int o, int t, double w) {
     super(o, t, w);
     this.wayId = wayId;
+    for (var v : TransportSpeed.values()) {
+      double timeWeight = (this.weight() / v.value);
+      timeWeights.put(v, timeWeight);
+    }
   }
 
   public Way(Integer wayId, SeparateChainingHashST<Tag, String> tags, int o, int t, double w) {
