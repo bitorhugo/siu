8a84dac3edc8e162dffbe784184ba0184bbb1dcc
Vitor <vitorhugosantos144@gmail.com>
2022-05-04 01:06:20 +0100

    fmxl working

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
 
+import java.io.IOException;
+
 import edu.princeton.cs.algs4.RedBlackBST;
 
 import main.edu.ufp.inf.en.lp2.*;
@@ -6,6 +8,8 @@ import main.edu.ufp.inf.en.lp2._1_intro.date.Date;
 import main.edu.ufp.inf.en.siu.*;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -15,13 +19,14 @@ import javafx.stage.Stage;
 public class App extends Application{
 
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("main/edu/ufp/inf/en/resources/test.fxml"));
+        
+        Scene scene = new Scene(root, 300, 300);
+        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
     
     public static void main(String[] args) throws Exception {    
