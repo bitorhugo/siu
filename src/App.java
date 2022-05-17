5c33a4f577571d02e83134dd28d9a9742b134c7e
Vitor <vitorhugosantos144@gmail.com>
2022-05-17 03:20:55 +0100

    added LoginController

--- src/App.java
+++ src/App.java
@@ -1,16 +1,5 @@
-
 import java.io.IOException;
-import java.time.LocalDate;
 
-import main.edu.ufp.inf.en.siu.IO.Arquive;
-import main.edu.ufp.inf.en.siu.IO.ArquiveBIN;
-import main.edu.ufp.inf.en.siu.IO.Upload;
-import main.edu.ufp.inf.en.siu.IO.UploadBIN;
-import main.edu.ufp.inf.en.siu.database.DataBase;
-import main.edu.ufp.inf.en.siu.database.poi.Poi;
-import main.edu.ufp.inf.en.siu.database.tag.Tag;
-import main.edu.ufp.inf.en.siu.map.Map;
-import main.edu.ufp.inf.en.siu.user.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -18,42 +7,28 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class App {
+public class App extends Application{
 
-    /*
+    
     @Override
-    public void start(Stage primaryStage) throws IOException{
+    public void start(Stage primaryStage) throws IOException {
         
-        Parent root = FXMLLoader.load(getClass().getResource("main/edu/ufp/inf/en/resources/test.fxml"));
-        
+        // load fxml file
+        Parent root = FXMLLoader.load(getClass().getResource("main/edu/ufp/inf/en/resources/Login.fxml"));
+
+        // set scene
         Scene scene = new Scene(root, 300, 300);
         primaryStage.setTitle("FXML Welcome");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
-    }*/
+
+    }
 
     
     
     public static void main(String[] args) throws Exception {    
-        
-        //launch();
-
-        DataBase db = new DataBase();
-        
-        Upload.Users(db);
-        Upload.Nodes(db);
-        Upload.Ways(db);
-        
-        User u = db.searchUser("38132");
-        System.out.println(u);
-        
-        Map map = new Map(db);
-        System.out.println(map.isConnected());
-        System.out.println(map.getGraph());
-        Map m1 = new Map (map, Tag.ALT_NAME);
-        System.out.println(m1.isConnected());
-        System.out.println(m1.getGraph());
+        launch();
     }
 
 }
